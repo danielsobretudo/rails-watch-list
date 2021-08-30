076e85be4c1d37cd715371c9e6f2bd4903159b9b
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-08-30 15:34:02 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+original_poster_url = "https://image.tmdb.org/t/p/original"
+
+puts "Importing movies"
+movies = JSON.parse(open(url).read)['results']
+movies.each do |movie|
+  puts "Creating #{movie['title']}"
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{original_poster_url}#{movie['backdrop_path']}",
+    rating: movie['vote_average']
+  )
+end
+puts "Movies created"
+
+# FROM SOLUTION:
+#
+# url = "http://tmdb.lewagon.com/movie/top_rated"
+# 10.times do |i|
+#   puts "Importing movies from page #{i + 1}"
+#   movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+#   movies.each do |movie|
+#     puts "Creating #{movie['title']}"
+#     base_poster_url = "https://image.tmdb.org/t/p/original"
+#     Movie.create(
+#       title: movie['title'],
+#       overview: movie['overview'],
+#       poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+#       rating: movie['vote_average']
+#     )
+#   end
+# end
+# puts "Movies created"
