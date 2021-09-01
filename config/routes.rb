3d479064a11e43c9aab63e8c8600c49a46070e47
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-09-01 08:26:30 -0300

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
